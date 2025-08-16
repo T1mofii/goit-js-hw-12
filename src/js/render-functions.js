701297aff9dbd85fe08a229b6a2ec7e6a11884e6
Timofii <tisenkot268@gmail.com>
2025-08-16 19:38:07 +0200

    add
    add

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,16 +14,26 @@ const lightbox = new SimpleLightbox('.gallery a', {
 export function createGallery(images) {
   const markup = images
     .map(
-      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-      <a class="gallery-item" href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
         <div class="info">
           <p><b>Likes:</b> ${likes}</p>
           <p><b>Views:</b> ${views}</p>
           <p><b>Comments:</b> ${comments}</p>
           <p><b>Downloads:</b> ${downloads}</p>
         </div>
-      </a>`
+      </li>`
     )
     .join('');
 
